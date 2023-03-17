e77ec3af3b0ccd4b86f87dd0807a3758cf28275d
nanvenomous <mrgarelli@gmail.com>
2023-03-16 22:49:17 -0500

    admin command `analyze` to understand userbase - teams data method to get breakdown of team sizes and task counts - developers data method to get devs and emails

--- responses.go
+++ responses.go
@@ -4,3 +4,14 @@ type TagDistribution struct {
 	Tag   string `json:"tag" bson:"_id"`
 	Count int    `json:"count" bson:"count"`
 }
+
+type MinimalDeveloper struct {
+	Name  string `bson:"name" json:"name"`
+	Email string `bson:"email" json:"email"`
+}
+
+type MinimalTeam struct {
+	Name       string   `bson:"name" json:"name"`
+	Developers []string `bson:"developers" json:"developers"`
+	TaskCount  uint64   `bson:"task_count" json:"task_count"`
+}
